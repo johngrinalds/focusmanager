7f22a1642df85a59c37ecfc709aea5a6f04d784b
John Grinalds <johngrinalds@gmail.com>
2024-06-13 06:54:42 -0400

    Initial Commit

---
+++ focusmanager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  focusmanager
+//
+//  Created by John Grinalds on 6/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ focusmanager/focusmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusmanagerApp.swift
+//  focusmanager
+//
+//  Created by John Grinalds on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct focusmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
