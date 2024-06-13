dfe201ba565dcb2f9d5fd21f1d7a2d9575693142
John Grinalds <johngrinalds@gmail.com>
2024-06-13 08:32:09 -0400

    Working access to arbitrary file via file picker

--- focusmanager/ContentView.swift
+++ focusmanager/ContentView.swift
@@ -6,12 +6,47 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    @State private var message: String = "Hello, World!"
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text(message)
+                .padding()
+                .frame(minWidth: 300, maxWidth: .infinity, minHeight: 150, maxHeight: .infinity)
+            
+            Button(action: {
+                // Get the URL of the file within the App Group container
+                
+                let dialog = NSOpenPanel()
+                        
+                dialog.title                   = "Choose a file"
+                dialog.showsResizeIndicator    = true
+                dialog.showsHiddenFiles        = false
+                dialog.allowsMultipleSelection = false
+                dialog.canChooseDirectories    = false
+                
+                if dialog.runModal() == NSApplication.ModalResponse.OK {
+                    if let fileURL = dialog.url {
+                        do {
+                            message = try String(contentsOfFile: fileURL.path, encoding: .utf8)
+                            print("File contents are: \(message)")
+                        } catch {
+                            print("Error reading file: \(error)")
+                        }
+                    }
+                } else {
+                    message = "No file selected"
+                }
+                
+                
+            }) {
+                Text("Click Me")
+            }
             .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
     }
 }
 
@@ -20,3 +55,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
