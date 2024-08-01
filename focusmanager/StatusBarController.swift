ce3162604ab26ab07b4ed7e06d75759cedc1ee6b
John Grinalds <johngrinalds@gmail.com>
2024-08-01 10:23:47 -0400

    Add status bar icon and functionality for dark mode

--- focusmanager/StatusBarController.swift
+++ focusmanager/StatusBarController.swift
@@ -8,12 +8,12 @@
 import AppKit
 import SwiftUI
 
-class StatusBarController: ObservableObject {
+class StatusBarController: NSObject, ObservableObject {
     private var statusBar: NSStatusBar
     private var statusItem: NSStatusItem
     private var setupComplete: Bool = false
 
-    init() {
+    override init() {
             self.statusBar = NSStatusBar.system
             self.statusItem = NSStatusItem()
         }
@@ -24,15 +24,17 @@ class StatusBarController: ObservableObject {
             setupComplete = true
             DispatchQueue.main.async {
                 self.statusItem = self.statusBar.statusItem(withLength: NSStatusItem.variableLength)
-                self.statusItem.button?.image = NSImage(named: NSImage.Name("16-mac-transparent"))
+                self.updateIcon()
                 self.statusItem.button?.image?.size = NSSize(width: 18, height: 18)
+                // Observe appearance changes
+                self.statusItem.button?.addObserver(self, forKeyPath: "effectiveAppearance", options: .new, context: nil)
             }
         }
     }
     
     func updateTitle(with time: String) {
         DispatchQueue.main.async {
-            self.statusItem.button?.image = nil
+            self.updateIcon()
             self.statusItem.button?.title = time
         }
     }
@@ -42,5 +44,23 @@ class StatusBarController: ObservableObject {
             self.statusItem.button?.image = NSImage(named: NSImage.Name("16-mac-transparent"))
             self.statusItem.button?.title = ""
         }
+    }
+    
+    private func updateIcon() {
+            let appearance = self.statusItem.button?.effectiveAppearance
+            let isDarkMode = appearance?.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
+            let imageName = isDarkMode ? "16-mac-transparent-dark" : "16-mac-transparent"
+            self.statusItem.button?.image = NSImage(named: NSImage.Name(imageName))
+        }
+        
+    // Observe appearance changes
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "effectiveAppearance" {
+            self.updateIcon()
+        }
+    }
+    
+    deinit {
+        self.statusItem.button?.removeObserver(self, forKeyPath: "effectiveAppearance")
     }
 }
